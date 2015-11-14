f469e36da547c3ab2be8910c985b221dd49beaac
Johann Tael <jntael@gmail.com>
2015-11-14 21:42:09 +0200

    fixed doc formatting

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate rustc_serialize;
 use std::mem;
 
 ///Generates a struct for entity data with required traits.
+///
 ///between {} should be a list of components formatted like this:
 ///ComponentType:component_array_name:id,
 ///id must be unique for each component and a power of two.
